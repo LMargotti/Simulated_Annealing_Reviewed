ff8e0d2ca8b5edbb06c72ae23337d7441536a25f
Lorenzo Margotti <lorenzo.margotti2@studio.unibo.it>
2021-04-17 02:23:32 +0200

    Added parsing + user function
    Modified and corrected run for parameters applications + generated user folder for personal function usage

---
+++ my_function.py
@@ -0,0 +1,12 @@
+#insert user function
+#import numpy as np
+INTERVAL=[-6,6]
+def chosen_function(X):
+    x=X[0]
+    y=X[1]
+    
+    #Insert your function of interest here, remember to change INTERVAL values if needed.#
+    #for example
+    return(x**2+y**2)
+
+    #pass
--- run.py
+++ run.py
@@ -1,8 +1,41 @@
 import algorithm as ag
 from core_functions import geom_cooling, boltz_acceptance_prob, boltz_move
 from test_functions import ackley_fn, rastrigin_fn, rosenbrock_fn, himmelblau_fn
+from my_function import chosen_function, INTERVAL
 
 if __name__ == '__main__':
+    #parsing
+    parser = argparse.ArgumentParser(description='Simulated Annealing Algorithm', formatter_class=argparse.RawTextHelpFormatter)
+    
+    parser.add_argument('-k', '--k_max', action='store', nargs='?', const=None, default=1e6, type=int,
+                        choices=None, help='Max number of iterations. [default: 1e6]', metavar=None)
+    
+    parser.add_argument('-m', '--mode', action='store', nargs='?', const=None, default='test', type=str,
+                        choices=None, help='[default: "test"]', metavar=None)
+    
+    parser.add_argument('-o', '--obj_fn_limit', action='store', nargs='?', const=None, default=-1e10, type=float,
+                        choices=None, help='Objective function limit. [default: -1e10]', metavar=None)
+    
+    parser.add_argument('-r', '--reann', action='store', nargs='?', const=None, default=100, type=float,
+                        choices=None, help='Reanniling tolerance value.[default: 100]', metavar=None)
+    
+    parser.add_argument('-t', '--init_temp', action='store', nargs='?', const=None, default=100, type=float,
+                        choices=None, help='Initial temperature. [default: 100]', metavar=None)
+    
+    parser.add_argument('-ti', '--tolerance_iter', action='store', nargs='?', const=None, default=1000, type=int,
+                        choices=None, help='Number of iterations taken into account in Tolerance Energy. [default: 1000]', metavar=None)
+    
+    parser.add_argument('-tv', '--tolerance_value', action='store', nargs='?', const=None, default=1e-6, type=float,
+                        choices=None, help='Tolerance Energy value for stopping criterion [default: 1e-6]', metavar=None)
+            
+    parser.add_argument('-v', '--verbose', action='store', nargs='?', const=None, default=False, type=bool,
+                        choices=None, help='[default: False]', metavar=None)
+        
+    args = parser.parse_args()
+    
+
+# Configuration mode: test functions are used for no "-m" command request.
+
     if args.mode == 'test':
 
         test_conf = {
@@ -23,8 +56,17 @@ if __name__ == '__main__':
             "Rastrigin" : [],
             "Rosenbrock" : []
         } 
-    else break
-    
+
+    elif args.mode == "userf"   
+        test_conf = {
+            "chosen_function" : [chosen_function, INTERVAL]
+        }
+        results = {"chosen_function" : []
+            }
+        exit_interations = {
+            "chosen_function" : []
+            }
+
     for fn, par in test_conf.items():
         states, energies, temp, k, _exit, reann = ag.simulated_annealing(
                                                         cooling = geom_cooling,
